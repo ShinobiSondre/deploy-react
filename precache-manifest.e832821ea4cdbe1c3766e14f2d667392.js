392940446ad73507e5a2651f3b64beb3cd17c068
ShinobiSondre <48123393+ShinobiSondre@users.noreply.github.com>
2020-03-26 14:28:23 +0100

    Updates

---
+++ precache-manifest.e832821ea4cdbe1c3766e14f2d667392.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "24b8d2b13ad8cd4a3f1ae7321ee43cee",
+    "url": "/deploy-react/index.html"
+  },
+  {
+    "revision": "7ecb5dcbfde852534639",
+    "url": "/deploy-react/static/css/main.27fd15f6.chunk.css"
+  },
+  {
+    "revision": "89c65cdab6d4b666672d",
+    "url": "/deploy-react/static/js/2.b81e40e1.chunk.js"
+  },
+  {
+    "revision": "59556d55448108d529f31fbb08c99a45",
+    "url": "/deploy-react/static/js/2.b81e40e1.chunk.js.LICENSE"
+  },
+  {
+    "revision": "7ecb5dcbfde852534639",
+    "url": "/deploy-react/static/js/main.af3bc4a8.chunk.js"
+  },
+  {
+    "revision": "56feab29083be1d3f0ec",
+    "url": "/deploy-react/static/js/runtime-main.3476c20f.js"
+  }
+]);
